d52fb0d8588c44132512ae6e75a3fb0aaa4aedca
Dominik Jaksch <dominik.jakschefg@gmail.com>
2023-07-11 22:57:23 +0200

    Changed Bot from pyautogui to an Pipeline drived system

--- main.py
+++ main.py
@@ -1,41 +1,67 @@
-import pyautogui
-import time
+import json
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-time.sleep(2)
+def find_skiprows(file_name):
+    with open(file_name, 'r', encoding='ISO-8859-1') as f:
+        for i, line in enumerate(f):
+            if 'R.Time (min)\tIntensity' in line:
+                return i
+    return None
 
-def locate_on_screen(image_path, confidence=0.9):
-    return pyautogui.locateOnScreen(image_path, confidence=confidence)
-    
-def click_image(image_path):
-    location = locate_on_screen(image_path)
-    if location:
-        print('I can see it')
-        x, y = pyautogui.center(location)
-        pyautogui.moveTo(x, y)
-        pyautogui.click(x, y)
-        return True
-    return False
+def analyze_hplc_data(file_name, retention_times):
+    # Find the number of rows to skip
+    skiprows = find_skiprows(file_name)
+    if skiprows is None:
+        print(f'Could not find data start in {file_name}')
+        return None
 
-# Stellen Sie sicher, dass die Anwendung geöffnet ist und warten Sie kurz
-time.sleep(2)
-    
-# Navigiere zum "File" Dropdown-Menü (Screenshot des "File" Textes)
-check = click_image("File_Dropdown.png")
-if check:
-    print('Hab es gefunden')
-else: 
-    print("Nicht gefunden")
-# Wartezeit, damit das Menü aufklappt
-time.sleep(5)
-    
-# Klicke auf den "Load Method Parameter" Button (Screenshot des Buttons)
-click_image("Load_Parameter.png")
-    
-# Wartezeit, damit das Eingabefenster erscheint
-time.sleep(5)
-    
-# Eingabe des Pfades im "File name" Inputfeld
-pyautogui.write("C:\\LabSolutions\\Data\\Shimadzu\\Labor MBT\\Messdaten\\Aminosäuren\\T1D_GABA\\Auswertung\\220810_GABA_OPA_Auswertung_Großversuch")
+    print(f'Loading data from {file_name} skipping {skiprows} lines')
 
-# Klicke auf den "Open" Button (Screenshot des Buttons)
-click_image("Open_Button.png")
+    # Load the HPLC data with 'ISO-8859-1' encoding and skipping the appropriate number of lines
+    hplc_data = pd.read_csv(file_name, sep='\t', encoding='ISO-8859-1', skiprows=skiprows)
+
+    print(f'Loaded data with columns: {hplc_data.columns}')
+
+    # Convert columns to appropriate data types
+    hplc_data['R.Time (min)'] = pd.to_numeric(hplc_data['R.Time (min)'], errors='coerce')
+    hplc_data['Intensity'] = pd.to_numeric(hplc_data['Intensity'], errors='coerce')
+
+    # Remove baseline noise
+    hplc_data['Intensity'] = hplc_data['Intensity'].apply(lambda x: x if x > 4 else 0)
+
+    # Select the peak between the specified retention times
+    peak_data = hplc_data[(hplc_data['R.Time (min)'] >= retention_times[0]) & (hplc_data['R.Time (min)'] <= retention_times[1])]
+
+    # Calculate the area under the peak
+    peak_area = np.trapz(peak_data['Intensity'], peak_data['R.Time (min)'])
+
+    # Plot the peak
+    plt.figure(figsize=(10, 6))
+    plt.plot(peak_data['R.Time (min)'], peak_data['Intensity'])
+    plt.fill_between(peak_data['R.Time (min)'], peak_data['Intensity'], color='skyblue', alpha=0.4)
+    plt.xlabel('Retention Time (min)')
+    plt.ylabel('Intensity')
+    plt.title('HPLC Peak')
+    plt.grid(True)
+    plt.savefig(file_name.replace('.txt', '_peak_plot.png'))
+
+    return peak_area
+
+# Load the analysis parameters from the JSON file
+with open('config/config.json', 'r') as f:
+    analysis_parameters = json.load(f)
+
+# Create a DataFrame to hold the results
+results = pd.DataFrame(columns=['Sample Name', 'Peak Area'])
+# Analyze each file
+for file_name in analysis_parameters['files']:
+    file_name = "RAW_DATA/" + file_name 
+    peak_area = analyze_hplc_data(file_name, analysis_parameters['retention_times'])
+    if peak_area is not None:
+        new_row = pd.DataFrame({'Sample Name': [file_name], 'Peak Area': [peak_area]})
+        results = pd.concat([results, new_row], ignore_index=True)
+
+# Save the results to a CSV file
+results.to_csv('Output/db/results.csv')
