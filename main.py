79cade2d286ec068168f393a7f5bd37d4defa34a
Dominik Jaksch <dominik.jakschefg@gmail.com>
2023-07-11 19:52:43 +0200

    Export to exe

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import time
 
 time.sleep(2)
 
-def locate_on_screen(image_path, confidence=0.8):
+def locate_on_screen(image_path, confidence=0.9):
     return pyautogui.locateOnScreen(image_path, confidence=confidence)
     
 def click_image(image_path):
     location = locate_on_screen(image_path)
     if location:
+        print('I can see it')
         x, y = pyautogui.center(location)
+        pyautogui.moveTo(x, y)
         pyautogui.click(x, y)
         return True
     return False
@@ -18,16 +20,19 @@ def click_image(image_path):
 time.sleep(2)
     
 # Navigiere zum "File" Dropdown-Menü (Screenshot des "File" Textes)
-click_image("File_Dropdown.png")
-    
+check = click_image("File_Dropdown.png")
+if check:
+    print('Hab es gefunden')
+else: 
+    print("Nicht gefunden")
 # Wartezeit, damit das Menü aufklappt
-time.sleep(2)
+time.sleep(5)
     
 # Klicke auf den "Load Method Parameter" Button (Screenshot des Buttons)
 click_image("Load_Parameter.png")
     
 # Wartezeit, damit das Eingabefenster erscheint
-time.sleep(2)
+time.sleep(5)
     
 # Eingabe des Pfades im "File name" Inputfeld
 pyautogui.write("C:\\LabSolutions\\Data\\Shimadzu\\Labor MBT\\Messdaten\\Aminosäuren\\T1D_GABA\\Auswertung\\220810_GABA_OPA_Auswertung_Großversuch")
